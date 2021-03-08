b708691d3d6905ba3fdea40f9b77bf2a34bbb900
bikashbist <bikas.bist@yahoo.com>
2021-03-08 07:42:36 +0545

    header

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paidal Yatri</title>
+    <link rel="stylesheet" href="css/test.css">
+    <script src="https://kit.fontawesome.com/25c99003c9.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <?php include 'navbar.php' ?>
+    <script src="js/app.js"></script>
+   
+</body>
+</html>
